31bc88d043eed7d11b9e4c27949392944477fd68
aluvse <dev.aluvse@gmail.com>
2025-08-29 13:05:20 +0600

    add get started funtionalities (not done yet 👷) 38:54

--- lib/presentation/intro/pages/get_started.dart
+++ lib/presentation/intro/pages/get_started.dart
@@ -1,10 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:spotify_clone_guys/core/configs/assets/app_images.dart';
+import 'package:spotify_clone_guys/core/configs/assets/app_vectors.dart';
+import 'package:spotify_clone_guys/core/configs/theme/app_colors.dart';
 
 class GetStartedPage extends StatelessWidget {
   const GetStartedPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage(AppImages.introBG),
+              ),
+            ),
+          ),
+
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Colors.black.withValues(alpha: 0.6),
+                  Colors.black.withValues(alpha: 0.2),
+                  Colors.transparent,
+                ],
+                begin: Alignment.bottomCenter,
+                end: Alignment.topCenter,
+              ),
+            ),
+          ),
+
+          SafeArea(
+            child: Column(
+              children: [
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: SvgPicture.asset(AppVectors.logo),
+                ),
+                Spacer(),
+                Text(
+                  'Enjoy Listening to Music',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontSize: 18,
+                  ),
+                ),
+                SizedBox(height: 21),
+                Text(
+                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sagittis enim purus sed phasellus. Cursus ornare id scelerisque aliquam.',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    color: AppColors.grey,
+                    fontSize: 13,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
